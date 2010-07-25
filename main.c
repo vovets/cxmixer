afae4ba5988946efc5dd8b436798b68eec8f5845
Vladimir Krasovskiy <vovets@gmail.com>
2010-07-25 05:54:31 +0400

    capturing input works, output works

--- main.c
+++ main.c
@@ -56,14 +56,6 @@ static void setup_io(void) {
     DDRB_DDB4 = 1;
 }
 
-static void enable_timer1(void) {
-    TCCR1_CS12 = 1;
-}
-
-static void setup_interrupts(void) {
-    PCMSK |= (1<<CH0BIT)+(1<<CH1BIT);
-}
-
 static void load_osccal(void) {
     OSCCAL = 0x6c;
 }
@@ -80,46 +72,81 @@ static u8_t calibration_mode;
 static u16_t period_current;
 static u16_t frq;
 static volatile u8_t output_completed;
+static volatile u8_t frame_captured;
 static u8_t output_enabled = 1;
 static union {
     u16_t v;
     struct {
-        u8_t low;
-        u8_t high;
+        u8_t l;
+        u8_t h;
     } b;
-} output_timer_u;
-static u8_t input_in_sync;
+} frame[4];
+u8_t input_timer_high;
+static union {
+    u16_t v;
+    struct {
+        u8_t l;
+        u8_t h;
+    } b;
+} output_timer;
 static i8_t delta = 1;
-static u16_t input_timer;
 static volatile u16_t ch0 = 1500;
 static u16_t ch1 = 1500;
 static u16_t period = 20000;
+static u8_t frame_bits;
 
 __eeprom struct eeprom_t calibration_data;
 
-static void produce_output(u16_t ch0, u16_t ch1, u16_t period);
+static void capture_frame(void);
+static void calculate_channels(void);
+static void produce_output(void);
 static void wait(u16_t width);
 static void setup_timer0(void);
 static void wait_for_output_comletion(void);
-static void produce_pulse_ch0(u16_t width);
+static void produce_pulse(u8_t ch, u16_t width);
 static void start_timer0(void);
 static void stop_timer0(void);
+static void start_timer1(void);
+static void stop_timer1(void);
 
+#pragma optimize=s none
 void main(void) {
     load_osccal();
     setup_io();
     __enable_interrupt();
     while(1) {
-        produce_output(ch0, ch1, period);
-        ch0 += delta;
-        if (ch0 < 1000 || ch0 > 2000)
-            delta = -delta;
+        capture_frame();
+        calculate_channels();
+        produce_output();
+        /* ch0 += delta; */
+        /* if (ch0 < 1000 || ch0 > 2000) */
+        /*     delta = -delta; */
     }
 }
 
-static void produce_output(u16_t ch0, u16_t ch1, u16_t period) {
-    produce_pulse_ch0(ch0);
-    wait(period - ch0);
+#pragma optimize=s none
+void capture_frame(void) {
+    input_timer_high = 0;
+    pinb = 0;
+    TIFR_TOV1 = 1;
+    frame_bits = 0;
+    PCMSK = (1<<CH0BIT) | (1<<CH1BIT);
+    GIFR_PCIF = 1;
+    start_timer1();
+    GIMSK_PCIE = 1;
+    while(!(frame_bits == 0x0f))
+        ;
+    stop_timer1();
+}
+
+void calculate_channels(void) {
+    ch0 = frame[1].v - frame[0].v;
+    ch1 = frame[3].v - frame[2].v;
+}
+
+static void produce_output() {
+    produce_pulse(0, ch0);
+    produce_pulse(1, ch1);
 }
 
 static void setup_timer0(void) {
@@ -129,16 +156,16 @@ static void setup_timer0(void) {
         tstart = 128,
         tmin = 50
     };
-    if (output_timer_u.b.low < tmin) {
-        u8_t adjust = tmin - output_timer_u.b.low;
+    if (output_timer.b.l < tmin) {
+        u8_t adjust = tmin - output_timer.b.l;
         TCNT0 = tstart + adjust;
-        OCR0A = adjust;
-        output_timer_u.b.low = tmin;
+        OCR0A = OCR0B = adjust;
+        output_timer.b.l = tmin;
     } else {
-        OCR0A = 0;
+        OCR0A = OCR0B = 0;
         TCNT0 = tstart;
     }
-    ++output_timer_u.b.high;
+    ++output_timer.b.h;
 }
 
 #pragma optimize=s none
@@ -147,19 +174,24 @@ static void wait_for_output_comletion(void) {
         ;
 }
 
-static void produce_pulse_ch0(u16_t width) {
-    output_timer_u.v = width;
+static void produce_pulse(u8_t ch, u16_t width) {
+    output_timer.v = width;
     setup_timer0();
     /* turn on ch on compare match */
-    TCCR0A_COM0A1 = 1;
-    TCCR0A_COM0A0 = 1;
+    if (ch) {
+        TCCR0A_COM0B1 = 1;
+        TCCR0A_COM0B0 = 1;
+    } else {
+        TCCR0A_COM0A1 = 1;
+        TCCR0A_COM0A0 = 1;
+    }
     output_completed = 0;
     start_timer0();
     wait_for_output_comletion();
 }
 
 static void wait(u16_t width) {
-    output_timer_u.v = width;
+    output_timer.v = width;
     setup_timer0();
     output_completed = 0;
     start_timer0();
@@ -170,12 +202,26 @@ static void start_timer0() {
     GTCCR_TSM = 1;
     GTCCR_PSR0 = 1;
     TCCR0B_CS01 = 1;
+    TIFR_TOV0 = 1;
     TIMSK_TOIE0 = 1;
     GTCCR_TSM = 0;
 }
 
 static void stop_timer0() {
     TCCR0B_CS01 = 0;
+}
+
+static void start_timer1() {
+    GTCCR_TSM = 1;
+    GTCCR_PSR1 = 1;
+    TCCR1_CS12 = 1;
+    TIFR_TOV1 = 1;
+    TIMSK_TOIE1 = 1;
+    GTCCR_TSM = 0;
+}
+
+static void stop_timer1() {
+    TCCR1_CS12 = 0;
 }
 
 /* interrupt handlers */
@@ -193,15 +239,16 @@ __interrupt void timer0_ovf(void);
 __interrupt void timer0_compa(void);
 
 __interrupt void timer1_ovf(void) {
-    input_timer += 256;
+    ++input_timer_high;
 }
 
 __interrupt void timer0_ovf(void) {
-    --output_timer_u.b.high;
-    if (!output_timer_u.b.high) {
-        OCR0A = output_timer_u.b.low;
-        TCCR0A_COM0A1 = 1;
-        TCCR0A_COM0A0 = 0;
+    --output_timer.b.h;
+    if (!output_timer.b.h) {
+        OCR0A = output_timer.b.l;
+        OCR0B = output_timer.b.l;
+        /* put outputs low on compare match */
+        TCCR0A = (1<<COM0A1) | (1<<COM0B1);
         TIMSK_OCIE0A = 1;
         TIMSK_TOIE0 = 0;
         TIFR_OCF0A = 1;
@@ -211,56 +258,64 @@ __interrupt void timer0_ovf(void) {
 __interrupt void timer0_compa(void) {
     TIMSK_OCIE0A = 0;
     stop_timer0();
-    TIFR_TOV0 = 1;
     output_completed = 1;
 }
 
 __interrupt void pcint(void) {
-//    u8_t tof = TIFR_TOV1;
-//    u8_t timer = TCNT1;
-//    if (tof) {
-//        input_timer += 256;
-//        TIFR_TOV1 = 1;
-//    }
-//    input_timer += timer;
-//    u8_t new_pinb = PINB;
-//    u8_t old_pinb = pinb;
-//    pinb = new_pinb;
-//    if (old_pinb & (1<<CH0BIT)) {
-//        /* was high */
-//        if (!(new_pinb & (1<<CH0BIT))) {
-//            /* falling edge */
-//            ++channels[0].mailbox.flag;
-//            channels[0].mailbox.value = input_timer;
-//            channels[0].mailbox.not_empty = 1;
-//            ++channels[0].mailbox.flag;
-//        }
-//    } else {
-//        if (new_pinb & (1<<CH0BIT)) {
-//            /* rising edge */
-//            TCNT1 = 0;
-//            input_timer = 0;
-//            input_in_sync = 1;
-//        }
-//    }
-//
-//    if (old_pinb & (1<<CH1BIT)) {
-//        /* was high */
-//        if (!(new_pinb & (1<<CH1BIT))) {
-//            /* falling edge */
-//            if (!input_in_sync)
-//                return;
-//            u16_t width = input_timer - channels[1].input.timer;
-//            ++channels[1].mailbox.flag;
-//            channels[1].mailbox.value = width;
-//            channels[1].mailbox.not_empty = 1;
-//            ++channels[1].mailbox.flag;
-//        }
-//    } else {
-//        if (new_pinb & (1<<CH1BIT)) {
-//            /* rising edge */
-//            channels[1].input.timer = input_timer;
-//        }
-//    }
+    u8_t tof = TIFR_TOV1;
+    u8_t timer = TCNT1;
+    if (tof || !timer) {
+        ++input_timer_high;
+        TIFR_TOV1 = 1;
+    }
+    u8_t new_pinb = PINB;
+    u8_t old_pinb = pinb;
+    pinb = new_pinb;
+    if (old_pinb & (1<<CH0BIT)) {
+        /* was high */
+        if (!(new_pinb & (1<<CH0BIT))) {
+            /* falling edge */
+            if (frame_bits & 0x01) {
+                frame[1].b.h = input_timer_high;
+                frame[1].b.l = timer;
+                frame_bits |= 0x02;
+            }
+        }
+    } else {
+        if (new_pinb & (1<<CH0BIT)) {
+            /* rising edge */
+            if (!(frame_bits & 0x01)) {
+                frame[0].b.h = input_timer_high;
+                frame[0].b.l = timer;
+                frame_bits |= 0x01;
+            }
+        }
+    }
 
+    if (old_pinb & (1<<CH1BIT)) {
+        /* was high */
+        if (!(new_pinb & (1<<CH1BIT))) {
+            /* falling edge */
+            if (frame_bits & 0x01) {
+                frame[3].b.h = input_timer_high;
+                frame[3].b.l = timer;
+                frame_bits |= 0x08;
+            }
+        }
+    } else {
+        if (new_pinb & (1<<CH1BIT)) {
+            /* rising edge */
+            if (frame_bits & 0x01) {
+                frame[2].b.h = input_timer_high;
+                frame[2].b.l = timer;
+                frame_bits |= 0x04;
+            }
+        }
+    }
+
+    if (frame_bits == 0x03) {
+        PCMSK = 0;
+        GIMSK_PCIE = 0;
+        GIFR_PCIF = 0;
+    }
 }
